71b1896cdc964aa80be5c2439333db7085399932
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-28 07:14:27 -0800

    will make the image rounded and use the overflow-hiddlen to make sure it does not overflow

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,10 +4,12 @@ export default function Home() {
       {/* User Information */}
       <div>
         {/* img */}
-        <img
-          src="https://scontent.fyvr1-1.fna.fbcdn.net/v/t31.18172-8/1490904_698721330168867_2069051041_o.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=AxvTGztWsIwAX840_u9&tn=AmocjGl2lpRv3oPr&_nc_ht=scontent.fyvr1-1.fna&oh=00_AfAZGZDjugXuB-UoWP5_iP5LtUwKAXbj0kbU-KjTtQml9g&oe=64257392"
-          alt="profile image"
-        />
+        <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+          <img
+            src="https://scontent.fyvr1-1.fna.fbcdn.net/v/t31.18172-8/1490904_698721330168867_2069051041_o.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=AxvTGztWsIwAX840_u9&tn=AmocjGl2lpRv3oPr&_nc_ht=scontent.fyvr1-1.fna&oh=00_AfAZGZDjugXuB-UoWP5_iP5LtUwKAXbj0kbU-KjTtQml9g&oe=64257392"
+            alt="profile image"
+          />
+        </div>
 
         {/* name */}
         <small>Daniel</small>
