aa6a84897ab4a53486ec6370137ec6a2c03d9cba
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-01 08:20:31 -0800

    start work on the expenses layout/section

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -7,10 +7,19 @@ export default function Home() {
         <small className="text-gray-400 text-md">My Balance</small>
         <h2 className="text-4xl font-bold">{currencyFormatter(98000)}</h2>
       </section>
-
       <section className="flex items-center gap-2 py-3">
         <button className="btn btn-primary">+ Expense</button>
         <button className="btn btn-primary-outline">+ Income</button>
+      </section>
+
+      {/* Expenses */}
+      <section className="py-6">
+        <h3 className="text-2xl">My Expenses</h3>
+        <div>
+          <div>
+            <div className="w-[25px] h-[25px] rounded-full bg-yellow-500" />
+          </div>
+        </div>
       </section>
     </main>
   );
