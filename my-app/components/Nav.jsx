2bc996af69acbcd05fcbf53260233387eed3c603
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-01 06:56:38 -0800

    contain a reusable component

---
+++ my-app/components/Nav.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ImStatsBars } from "react-icons/im";
+
+const Nav = () => {
+  return (
+    <header className="container max-w-2xl px-6 py-6 mx-auto">
+      <div className="flex items-center justify-between">
+        {/* User Information */}
+        <div className="flex items-center gap-2">
+          {/* img */}
+          <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+            <img
+              className="w-full h-full object-cover"
+              src="https://scontent.fyvr1-1.fna.fbcdn.net/v/t31.18172-8/1490904_698721330168867_2069051041_o.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=AxvTGztWsIwAX840_u9&tn=AmocjGl2lpRv3oPr&_nc_ht=scontent.fyvr1-1.fna&oh=00_AfAZGZDjugXuB-UoWP5_iP5LtUwKAXbj0kbU-KjTtQml9g&oe=64257392"
+              alt="profile image"
+            />
+          </div>
+
+          {/* name */}
+          <small>Daniel</small>
+        </div>
+
+        {/* Right side of our navigation */}
+        <nav className="flex item-center gap-2">
+          <div>
+            <ImStatsBars className="text-2xl" />
+          </div>
+          <div>
+            <button className="btn btn-danger">Sign out</button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
