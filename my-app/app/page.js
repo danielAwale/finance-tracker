793a40357b73fc5c6919fe20279e629c07532a95
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-01 07:12:27 -0800

    contain button styling

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <main className="container mac-w-2xl px-6 py-6 mx-auto">
-      <section>
+      <section className="py-3">
         <small className="text-gray-400 text-md">My Balance</small>
         <h2 className="text-4xl font-bold">$98000</h2>
       </section>
 
-      <section>
-        <button className="btn">+ Expense</button>
-        <button className="btn">+ Income</button>
+      <section className="flex items-center gap-2 py-3">
+        <button className="btn btn-primary">+ Expense</button>
+        <button className="btn btn-primary-outline">+ Income</button>
       </section>
     </main>
   );
