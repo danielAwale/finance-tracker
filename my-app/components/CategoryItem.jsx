2d011f2b411cd16481266671f2289dcb326591a1
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-01 08:31:56 -0800

    create and work on the categoryitem component

---
+++ my-app/components/CategoryItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CategoryItem = ({ color, title, amount }) => {
+  return (
+    <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+      <div className="flex items-center gap-2">
+        <div
+          className="w-[25px] h-[25px] rounded-full"
+          style={{ backgroundColor: color }}
+        />
+        <h4 className="capitalize">{title}</h4>
+      </div>
+      <p>{currencyFormatter(amount)}</p>
+    </div>
+  );
+};
+
+export default CategoryItem;
