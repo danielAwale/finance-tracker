f94ca14807ab0a4c7bb595a5d106753ddd78cf80
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-01 07:04:19 -0800

    will contain some sections

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <section></section>;
+  return (
+    <main className="container mac-w-2xl px-6 py-6 mx-auto">
+      <section>
+        <small className="text-gray-400 text-md">My Balance</small>
+        <h2 className="text-4xl font-bold">$98000</h2>
+      </section>
+
+      <section>
+        <button className="btn">+ Expense</button>
+        <button className="btn">+ Income</button>
+      </section>
+    </main>
+  );
 }
