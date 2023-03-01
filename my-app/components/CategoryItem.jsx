208337f1ac1502a332761bd03f1f5aa4d336a40b
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-01 08:45:11 -0800

    wrap the entire category item in a button element to render the hover effect

--- my-app/components/CategoryItem.jsx
+++ my-app/components/CategoryItem.jsx
@@ -3,16 +3,18 @@ import { currencyFormatter } from "@/lib/utils";
 
 const CategoryItem = ({ color, title, amount }) => {
   return (
-    <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
-      <div className="flex items-center gap-2">
-        <div
-          className="w-[25px] h-[25px] rounded-full"
-          style={{ backgroundColor: color }}
-        />
-        <h4 className="capitalize">{title}</h4>
+    <button>
+      <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+        <div className="flex items-center gap-2">
+          <div
+            className="w-[25px] h-[25px] rounded-full"
+            style={{ backgroundColor: color }}
+          />
+          <h4 className="capitalize">{title}</h4>
+        </div>
+        <p>{currencyFormatter(amount)}</p>
       </div>
-      <p>{currencyFormatter(amount)}</p>
-    </div>
+    </button>
   );
 };
 
