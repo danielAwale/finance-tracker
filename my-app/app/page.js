465e594af0e64c3da642ee86d8064a6172e6c69b
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-03 16:15:42 -0800

    contain the styles for the modal

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -44,52 +44,60 @@ const dummyData = [
 
 export default function Home() {
   return (
-    <main className="container mac-w-2xl px-6 py-6 mx-auto">
-      <section className="py-3">
-        <small className="text-gray-400 text-md">My Balance</small>
-        <h2 className="text-4xl font-bold">{currencyFormatter(6000)}</h2>
-      </section>
-      <section className="flex items-center gap-2 py-3">
-        <button className="btn btn-primary">+ Expense</button>
-        <button className="btn btn-primary-outline">+ Income</button>
-      </section>
+    <>
+      {/* Modal */}
+      <div className="absolute top-0 left-0 w-full h-full">
+        <div className="container mx-auto max-w-2xl h-[80vh] rounded-3xl bg-slate-800"></div>
+      </div>
+      <main className="container mac-w-2xl px-6 py-6 mx-auto">
+        <section className="py-3">
+          <small className="text-gray-400 text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(6000)}</h2>
+        </section>
+        <section className="flex items-center gap-2 py-3">
+          <button className="btn btn-primary">+ Expense</button>
+          <button className="btn btn-primary-outline">+ Income</button>
+        </section>
 
-      {/* Expenses */}
-      <section className="py-6">
-        <h3 className="text-2xl">My Expenses</h3>
-        <div className="flex flex-col gap-4 mt-6">
-          {dummyData.map((item) => {
-            return (
-              <CategoryItem
-                color={item.color}
-                title={item.title}
-                amount={item.total}
-              />
-            );
-          })}
-        </div>
-      </section>
+        {/* Expenses */}
+        <section className="py-6">
+          <h3 className="text-2xl">My Expenses</h3>
+          <div className="flex flex-col gap-4 mt-6">
+            {dummyData.map((item) => {
+              return (
+                <CategoryItem
+                  color={item.color}
+                  title={item.title}
+                  amount={item.total}
+                />
+              );
+            })}
+          </div>
+        </section>
 
-      {/* Chart Section */}
-      <section className="py-6 px-6">
-        <h3 className="text-2xl">Stats</h3>
-        <div className="w-1/2 mx-auto">
-          <Doughnut
-            data={{
-              labels: dummyData.map((expense) => expense.title),
-              datasets: [
-                {
-                  label: "Expenses",
-                  data: dummyData.map((expenses) => expenses.total),
-                  backgroundColor: dummyData.map((expenses) => expenses.color),
-                  borderColor: ["#18181b"],
-                  borderWidth: 5,
-                },
-              ],
-            }}
-          />
-        </div>
-      </section>
-    </main>
+        {/* Chart Section */}
+        <section className="py-6 px-6">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: dummyData.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: dummyData.map((expenses) => expenses.total),
+                    backgroundColor: dummyData.map(
+                      (expenses) => expenses.color
+                    ),
+                    borderColor: ["#18181b"],
+                    borderWidth: 5,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
