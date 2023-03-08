e4aac604f4a90d6c5bf730b12b7b9a440e55e889
danielAwale <danielmadhukarawale@gmail.com>
2023-03-08 09:19:51 -0800

    make sure everything works well so far. minor error issues will fix later

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -70,7 +70,7 @@ export default function Home() {
       <main className="container mac-w-2xl px-6 py-6 mx-auto">
         <section className="py-3">
           <small className="text-gray-400 text-md">My Balance</small>
-          <h2 className="text-4xl font-bold">{currencyFormatter(6000)}</h2>
+          <h2 className="text-4xl font-bold">{currencyFormatter(10000)}</h2>
         </section>
         <section className="flex items-center gap-2 py-3">
           <button className="btn btn-primary" onClick={() => { }}>+ Expense</button>
